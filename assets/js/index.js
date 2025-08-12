b8ac3b20d76f9d0b21f43503fd608b7fae2127d1
Erick Wainaina <erickgitahi95@gmail.com>
2025-08-12 23:00:53 +0300

    Add: remaining styles on index page
    finnished project's contact page as well

--- assets/js/index.js
+++ assets/js/index.js
@@ -39,3 +39,75 @@ closeNav.addEventListener('click', () => {
     closeNav.classList.remove('active');
     menuToggle.classList.remove('active');
 });
+
+
+// fetching data from db.json
+const data = fetch('/assets/js/db.json')
+    .then(response => response.json())
+    .then(data => {
+        const projectsContainer = document.querySelector('.projects');
+        data.projects.forEach(project => {
+            const projectElement = document.createElement('div');
+            projectElement.classList.add('project');
+            projectElement.innerHTML = `
+            <div class="top">
+                    <h3>${project.title}</h3>
+                    <div class="time-line">
+                        <span class="start-date">Start Date /</span>
+                        <span class="end-date">End Date</span>
+                        <h4>${project.start} - ${project.end}</h4>
+                    </div>
+                </div>
+                <div class="description">
+                    <p>${project.description}</p>
+                </div>
+                <div class="tech">
+                    <ul class="tech-stack">
+                        ${project.techStack.map(tech => `
+                            <li class="tech-icon">${tech}</li>
+                        `).join('')}
+                    </ul>
+                    <div class="links">
+                        <ul>
+                            ${project.link != null ? 
+                                `<li><a href="${project.link}" target="_blank">Project Link</a></li>` : ''
+                            }
+                            <li><a href="${project.github}" target="_blank">GitHub Link</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            `;
+            projectsContainer.appendChild(projectElement);
+        });
+    })
+
+    // <div class="project">
+    //     <div class="top">
+    //         <h3>Project Title</h3>
+    //         <div class="time-line">
+    //             <span class="start-date">Start Date /</span>
+    //             <span class="end-date">End Date</span>
+    //             <h4>Feb/2025 - Dec/2025</h4>
+    //         </div>
+    //     </div>
+    //     <div class="description">
+    //         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi debitis, quisquam veritatis molestiae mollitia numquam officia rerum quasi maxime obcaecati omnis consequatur cum optio facere nemo non deleniti dolorum quod.
+    //         Maiores quod animi, quis natus quidem inventore corporis explicabo qui est esse. Natus vitae nam veritatis est quis nisi enim assumenda dolor incidunt reiciendis, rerum adipisci pariatur voluptatem maxime quia.
+    //         Corrupti voluptas dicta sit possimus soluta, culpa non voluptates obcaecati facere ad similique harum, est sed ut laboriosam incidunt exercitationem deleniti eligendi? Reprehenderit reiciendis ratione, sit dolor eius consectetur aperiam.</p>
+    //     </div>
+    //     <div class="tech">
+    //         <ul class="tech-stack">
+    //             <li class="tech-icon"><img src="/assets/images/react.svg" alt=""></li>
+    //             <li class="tech-icon"><img src="/assets/images/react.svg" alt=""></li>
+    //             <li class="tech-icon"><img src="/assets/images/react.svg" alt=""></li>
+    //             <li class="tech-icon"><img src="/assets/images/react.svg" alt=""></li>
+    //         </ul>
+    //         <div class="links">
+    //             <ul>
+    //                 <li><a href="#">Project Link</a></li>
+    //                 <li><a href="#">GitHub Link</a></li>
+    //             </ul>
+    //         </div>
+    //     </div>
+    // </div>
